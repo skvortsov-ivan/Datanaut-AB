4f33c7b8e96ab32845ade966ebfd82870041b6df
Melvin Danielsson <126797817+Melle1010@users.noreply.github.com>
2025-12-02 14:05:14 +0100

    Add files via upload

---
+++ Views/SQLQuery11.sql
@@ -0,0 +1,18 @@
+USE [DataNautDB]
+GO
+
+/****** Object:  View [dbo].[V_EmployeesInProject]    Script Date: 12/2/2025 2:03:24 PM ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE OR ALTER VIEW [dbo].[V_EmployeesInProject] AS
+SELECT p.ProjectId, p.ProjectName AS 'Project Name', e.EmployeeId, e.FullName AS 'Full Name', e.EmployeeRole AS 'Employee Role'
+FROM Project p 
+JOIN ProjectEmployee pe ON p.ProjectId=pe.FkProjectId
+JOIN Employee e ON pe.FkEmployeeId=e.EmployeeId;
+GO
+
+
